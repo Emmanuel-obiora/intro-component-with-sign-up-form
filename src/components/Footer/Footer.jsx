6d3b5f03fba3f2883f9aa3893a04adc1f154a128
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-02-11 17:41:44 +0100

    Updating js code to relevant react compactibles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import '../../style.css'
 const Footer = () => {
     return (
         <footer>
-            <p class="attribution">
+            <p className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel='noreferrer' target="_blank">Frontend Mentor</a>. 
             Coded by <a href="https://emmanuel-obiora.github.io/portfolio-about-me-/src" rel='noreferrer' target="_blank">Obiora Emmanuel</a>.
             </p>
--- src/components/Right/Right.jsx
+++ src/components/Right/Right.jsx
@@ -4,78 +4,57 @@ import errorIcon from '../../images/icon-error.svg'
 
 const Right = () => {
 
-    const formData = document.getElementById('signUp');
-
     const sendEmail = (e) => {
         e.preventDefault();
 
-        const first = formData['Fname'].value;
-        const last = formData['Lname'].value;
-        const email = formData['Email'].value;
-        const password = formData['Pass'].value;
-
-        const icon = document.getElementById('nameF');
-        const icon2 = document.getElementById('nameL');
-        const icon3 = document.getElementById('mailI');
-        const icon4 = document.getElementById('passwordI');
+        const first = document.getElementById('Fname').value;
+        const last = document.getElementById('Lname').value;
+        const email = document.getElementById('Email').value;
+        const password = document.getElementById('Pass').value;
 
         if (first === ""){
-            addErrorTo('Fname', 'First name cannot be empty');
-            icon.style.display = 'block';
+            const changer = document.getElementById('Fname').parentNode;
+            changer.classList.add('error-input');
         } 
         else{
-            removeErrorFrom('Fname');
-            icon.style.display = 'none';
+            const changer = document.getElementById('Fname').parentNode;
+            changer.classList.remove('error-input');
         }
 
         if (last === ""){
-            addErrorTo('Lname', 'last name cannot be empty');
-            icon2.style.display = 'block';
+            const changer = document.getElementById('Lname').parentNode;
+            changer.classList.add('error-input');
         } 
         else {
-            removeErrorFrom('Lname');
-            icon2.style.display = 'none';
+            const changer = document.getElementById('Lname').parentNode;
+            changer.classList.remove('error-input');
         }
 
         if (email === ""){
-            addErrorTo('Email', 'email cannot be empty');
-            icon3.style.display = 'block';
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.add('error-input');
         } 
         else if (!isValid(email)){
-            addErrorTo('Email', 'Looks like this is not an email');
-            icon3.style.display = 'block';
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.add('error-input');
+
+            const chgText = document.getElementById('chgText');
+            chgText.innerText = "This is not a valid email";
         } 
         else {
-            removeErrorFrom('Email');
-            icon3.style.display = 'none';
+            const changer = document.getElementById('Email').parentNode;
+            changer.classList.remove('error-input');
         }
 
         if (password === ""){
-            addErrorTo('Pass', 'password cannot be empty');
-            icon4.style.display = 'block';
+            const changer = document.getElementById('Pass').parentNode;
+            changer.classList.add('error-input');
         } else {
-            removeErrorFrom('Pass');
-            icon4.style.display = 'none';
+            const changer = document.getElementById('Pass').parentNode;
+            changer.classList.remove('error-input');
         }
         
     };
-
-    function addErrorTo(field, message){
-        const inputControl = formData[field].parentNode;
-        inputControl.classList.add('error-input');
-
-        const small = inputControl.querySelector('small');
-        small.innerText = message;
-        small.style.display = 'block';
-    }
-
-    function removeErrorFrom(field){
-        const inputControl = formData[field].parentNode;
-        inputControl.classList.remove('error-input');
-
-        const small = inputControl.querySelector('small');
-        small.style.display = 'none';
-    }
 
     function isValid(email) {
         var re = /\S+@\S+\.\S+/;
@@ -89,22 +68,22 @@ const Right = () => {
                 <div className='right-form_input-field'>
                     <input type="text" id='Fname' placeholder='First Name'/>
                     <img src={errorIcon} className="error" id="nameF" alt="" />
-                    <small></small>
+                    <small>First name cannot be empty</small>
                 </div>
                 <div className='right-form_input-field'>
                     <input type="text" id='Lname' placeholder='Last Name' />
                     <img src={errorIcon} className="error" id="nameL" alt="" />
-                    <small ></small>
+                    <small >last name cannot be empty</small>
                 </div>
                 <div className='right-form_input-field'>
                     <input type="email" id='Email' placeholder='Email Address' />
                     <img src={errorIcon} className="error" id='mailI' alt="" />
-                    <small></small>
+                    <small id="chgText">Email cannot be empty</small>
                 </div>
                 <div className='right-form_input-field'>
                     <input type="password" id='Pass' placeholder='Password' />
                     <img src={errorIcon} className="error" id='passwordI' alt="" />
-                    <small></small>
+                    <small>Password cannot be empty</small>
                 </div>
                 <button type='submit' className='right-form_submit' >Claim your free trial</button>
                 <p className='right-form_para'>By clicking the button, you are agreeing to our <span className='red-para'>Terms and Services</span></p>
