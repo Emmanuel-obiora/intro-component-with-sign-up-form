f195b323e618578eeafad9c41b5de47639428ec6
Emmanuel-Obiora <emmanuelobiora94@gmail.com>
2023-02-11 07:46:30 +0100

    Adding mobile responsiveness

--- src/components/Right/Right.jsx
+++ src/components/Right/Right.jsx
@@ -84,7 +84,7 @@ const Right = () => {
 
     return (
         <div className='right'>
-            <strong className='right-top'>Try it free 7 days<span className="right-top_change">then $20/mo. thereafter </span></strong>
+            <h2 className='right-top'>Try it free 7 days<span className="right-top_change">then $20/mo. thereafter </span></h2>
             <form action="" className='right-form' id="signUp" onSubmit={sendEmail}>
                 <div className='right-form_input-field'>
                     <input type="text" id='Fname' placeholder='First Name'/>
